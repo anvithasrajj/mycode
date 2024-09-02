4335767e4eba9c55a8472dc4fee2723849e58ed6
anvithasrajj <138256381+anvithasrajj@users.noreply.github.com>
2024-09-02 15:30:12 +0100

    Create contacts_041.py

---
+++ contacts_041.py
@@ -0,0 +1,14 @@
+#1/usr/bin/env python3
+
+contacts = {'Sue' : '085-6442378' , 'Jimmy' : '086-1223277' , 'Maggie' : '087-8822001' , 'Amy' : '087-3240516'}
+
+import sys
+line = readline.stdin
+#print(contacts)
+#print(contacts.items())
+
+if contacts.items()[0]
+
+for k, v in contacts.items(), key = tagger:
+  print(f'Name: {contacts[0]})
+  print(f'Phone: {contacts[1]})
